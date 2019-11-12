673eb44b7c9664b93d6f35f9e27e48e76fed3069
Abhijith K <iamabhijith.k@gmail.com>
2019-11-12 15:35:01 +0530

    add about section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/nav';
+import About from './components/about';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <About/>
     </div>
   );
 }
