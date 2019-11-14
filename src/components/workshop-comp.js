7685171d005cf6c304209b8aebcca1913fa2ad67
Abhijith K <iamabhijith.k@gmail.com>
2019-11-14 16:16:23 +0530

    added state functions

--- src/components/workshop-comp.js
+++ src/components/workshop-comp.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react'
 import './workshops.css'
 class WorkshopComp extends Component{
+    constructor(){
+        super()
+        this.state = {
+            message : "Register for Workshop"
+        }
+    }
+    registerWorkshop(){
+        this.setState({
+            message:'Added to Cart',
+        })
+        console.log("State changed with setState", this.state.message)
+    }
+    registerWorkshopDir(){
+        this.state.message="New Message"
+        console.log("State directly assigned",this.state.message)
+    }
     render(){
         return(
             <div className="workshop-comp">
                 <h2>{this.props.name}</h2>
-                <h3>{this.props.fees}</h3>
-                <h4>{this.props.venue}</h4>
+                <h3>{this.props.venue}</h3>
+                <h4>{this.props.fees}</h4>
+                <h4>{this.state.message}</h4>
+                <button onClick={()=>this.registerWorkshop()}>Register</button>
             </div>
         );
     }
